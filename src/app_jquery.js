3ded1df59cc12ceaebaa88ee67acb524c7a0ee52
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-18 17:49:37 +0100

    Beginning to write code for ajax request - not finished yet

--- src/app_jquery.js
+++ src/app_jquery.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
   thermostat = new Thermostat();
 
+  $(".get_weather").click(function() {
+    var request = getWeather("London");
+
+    if(request.status == 304 || request.status == 200) {
+      //to be continued...
+    }
+  });
+
+  function getWeather(city) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=London,uk");
+    xhr.send();
+    return xhr;
+  }
+
   temperatureColor = function() {
     $("span").removeClass();
     $("span").addClass(thermostat.setColour());
