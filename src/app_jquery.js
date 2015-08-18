f57f28f6bed714aeb27185681dc08a4eeb0f582e
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-18 20:51:47 +0100

    First part of api call done - need to jquery-fy things and allow for any city

--- src/app_jquery.js
+++ src/app_jquery.js
@@ -1,30 +1,40 @@
 $(document).ready(function() {
+  // APIKEY = ENV['OPEN_WEATHER_KEY'];
   thermostat = new Thermostat();
 
-  $(".get_weather").click(function() {
-    var request = getWeather("London");
+  $(".get_weather").click(function(event) {
+    event.preventDefault();
+    // var city = $(".get_weather").val();
+    var request = getWeatherInfo();
 
     if(request.status == 304 || request.status == 200) {
-      //to be continued...
+      showWeather(JSON.parse(request.responseText));
+    }
+    else {
+      return "There was an error.";
     }
   });
 
-  function getWeather(city) {
+  function getWeatherInfo() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=London,uk");
+    xhr.open("GET", "http://api.openweathermap.org/data/2.5/weather?id=2643743&APPID={e3771f9e183904f9fa76308c7ef5505c}", false);
     xhr.send();
     return xhr;
-  }
+  };
 
-  temperatureColor = function() {
+  function showWeather(weather_info) {
+    $(".weather").text(weather_info.weather[0].description);
+  };
+
+  function temperatureColor() {
     $("span").removeClass();
     $("span").addClass(thermostat.setColour());
-  }
+  };
 
-  update = function() {
+  function update() {
     $("span").text(thermostat.temperature);
     temperatureColor();
-  }
+  };
 
   update();
 
