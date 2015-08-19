21c7371bc6010f973f464d2cd3384d9fca550090
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-19 12:03:00 +0100

    Written feature tests for jquery

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -1,6 +1,6 @@
-describe('Thermostat', function() {
-  beforeEach(function() {
-    beforeEach(function(){
+describe('Thermostat',function(){
+
+  beforeEach(function(){
     jasmine.getFixtures().fixturesPath = '.';
     loadFixtures('index.html');
     $.holdReady(false);
@@ -11,7 +11,86 @@ describe('Thermostat', function() {
   });
 
   it("should increase temperature when you click the increase button", function() {
-    $(".increase").click;
+    $(".increase").click();
     expect("span").toContainText("21");
   });
+
+  it("should decrease temperature when you click the decrease button", function() {
+    $(".decrease").click();
+    expect("span").toContainText("19");
+  });
+
+  it("should be able to change the temperature up and down consistently", function() {
+    $(".increase").click();
+    expect("span").toContainText("21");
+    $(".decrease").click();
+    expect("span").toContainText("20");
+  });
+
+  it("should be able to reset the temperature", function() {
+    $(".increase").click();
+    $(".reset").click();
+    expect("span").toContainText("20");
+  });
+
+
+  it("should have a minimum temperature of 10 degrees", function() {
+    expect(function() {
+      for (var i = 0; i < 11; i++) {
+        $(".decrease").click();
+      }
+    }).toThrowError("Temperature can not go below minimum.");
+    expect("span").toContainText("10");
+  });
+
+  it("should cap the maximum at 25 degrees when power saving mode is turned on", function() {
+    expect(function() {
+      for (var i = 0; i < 7; i++) {
+        $(".increase").click(); }
+      }).toThrowError("Temperature can not go above maximum.");
+    expect("span").toContainText("25");
+  });
+
+  it("should cap the maximum at 32 degrees when power saving mode is turned off", function() {
+    $("input:checkbox").change();
+    expect(function() {
+      for (var i = 0; i < 14; i++) {
+        $(".increase").click();
+      }
+    }).toThrowError("Temperature can not go above maximum.");
+    expect("span").toContainText("32");
+  });
+
+  it("should reset temperature when power saving mode is switched from off to on", function() {
+    $("input:checkbox").change();
+    $(".increase").click();
+    $("input:checkbox").change();
+    expect("span").toContainText("20");
+  });
+
+  it("should have a green temperature if below 18 degrees", function() {
+    for (var i = 0; i < 3; i++) {
+      $(".decrease").click();
+    }
+    expect("span").toHaveClass("text--green");
+    expect($("span").css("color")).toBe("rgb(0, 128, 0)");
+  });
+
+  it("should have an orange temperature if below 25 degrees", function() {
+    expect("span").toHaveClass("text--orange");
+    expect($("span").css("color")).toBe("rgb(255, 165, 0)");
+  });
+
+  it("should have a red temperature if above 25 degrees", function() {
+    for (var i = 0; i < 5; i++) {
+      $(".increase").click();
+    }
+    expect("span").toHaveClass("text--red");
+    expect($("span").css("color")).toBe("rgb(255, 0, 0)");
+  })
+
+  it("should have power saving mode checked at start", function() {
+    expect("input:checkbox").toHaveProp("checked");
+  });
+
 });
